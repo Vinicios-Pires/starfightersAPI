e32819f8c14325c8e5e67ecb3241b0a643cc40d6
Vinicios Pires <contato.viniciospires@gmail.com>
2022-07-07 14:34:47 -0300

    feat: battle router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import rankingRouter from "./rankingRouter.js"
+import battleRouter from "./battleRouter.js"
 
 const router = Router();
 
 router.use(rankingRouter);
+router.use(battleRouter);
 
 export default router;
